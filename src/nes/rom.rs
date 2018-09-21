49dfce05cd14addc47f2540abfbd089a34d113df
Federico Ravasio <ravasio.federico@gmail.com>
2018-09-21 17:22:50 +0400

    Linting.

--- src/nes/rom.rs
+++ src/nes/rom.rs
@@ -39,26 +39,27 @@ impl Rom {
                 >> flags_10: le_u8
                 >> take!(5)
                 >> prg: take!(16_384 * size_prg_rom as usize)
-                >> chr: take!(8_192 * size_chr_rom as usize) >> ({
-                let lo_mapper = flags_6 & 0x0F;
-                let hi_mapper = flags_7 & 0x78;
+                >> chr: take!(8_192 * size_chr_rom as usize)
+                >> ({
+                    let lo_mapper = flags_6 & 0x0F;
+                    let hi_mapper = flags_7 & 0x78;
 
-                Rom {
-                    flags_6,
-                    flags_7,
-                    size_prg_ram: if size_prg_ram == 0 {
-                        1
-                    } else {
-                        size_prg_ram
-                    },
-                    flags_9,
-                    flags_10,
-                    prg: prg.to_vec(),
-                    chr: chr.to_vec(),
-                    mapper: lo_mapper | hi_mapper,
-                    sram: [0; 0x2000],
-                }
-            })
+                    Rom {
+                        flags_6,
+                        flags_7,
+                        size_prg_ram: if size_prg_ram == 0 {
+                            1
+                        } else {
+                            size_prg_ram
+                        },
+                        flags_9,
+                        flags_10,
+                        prg: prg.to_vec(),
+                        chr: chr.to_vec(),
+                        mapper: lo_mapper | hi_mapper,
+                        sram: [0; 0x2000],
+                    }
+                })
         )
     );
 }
