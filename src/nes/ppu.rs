00b0fb17973f30f64327d878d6055e53885c03ec
Federico Ravasio <ravasio.federico@gmail.com>
2018-10-02 12:51:08 +0400

    Reorganized PPU struct layout and added doc about registers.

--- src/nes/ppu.rs
+++ src/nes/ppu.rs
@@ -20,13 +20,51 @@ impl Frame {
 
 #[derive(Clone, Copy)]
 pub struct Ppu {
-    control: Control,
-    mask: Mask,
-    cycle: u16,
     scanline: u16,
+    cycle: u16,
     vblank: bool,
     frame: Frame,
+
+    /// $2000 w
+    ///
+    /// 7  bit  0
+    /// ---- ----
+    /// VPHB SINN
+    /// |||| ||||
+    /// |||| ||++- Base nametable address
+    /// |||| ||    (0 = $2000; 1 = $2400; 2 = $2800; 3 = $2C00)
+    /// |||| |+--- VRAM address increment per CPU read/write of PPUDATA
+    /// |||| |     (0: add 1, going across; 1: add 32, going down)
+    /// |||| +---- Sprite pattern table address for 8x8 sprites
+    /// ||||       (0: $0000; 1: $1000; ignored in 8x16 mode)
+    /// |||+------ Background pattern table address (0: $0000; 1: $1000)
+    /// ||+------- Sprite size (0: 8x8; 1: 8x16)
+    /// |+-------- PPU master/slave select
+    /// |          (0: read backdrop from EXT pins; 1: output color on EXT pins)
+    /// +--------- Generate an NMI at the start of the
+    /// vertical blanking interval (0: off; 1: on)
+    control: Control,
+
+    /// $2001 w
+    ///
+    /// 7  bit  0
+    /// ---- ----
+    /// BGRs bMmG
+    /// |||| ||||
+    /// |||| |||+- Greyscale (0: normal color, 1: produce a greyscale display)
+    /// |||| ||+-- 1: Show background in leftmost 8 pixels of screen, 0: Hide
+    /// |||| |+--- 1: Show sprites in leftmost 8 pixels of screen, 0: Hide
+    /// |||| +---- 1: Show background
+    /// |||+------ 1: Show sprites
+    /// ||+------- Emphasize red*
+    /// |+-------- Emphasize green*
+    /// +--------- Emphasize blue*
+    mask: Mask,
+
+    /// $2003 w
     oam_addr: u8,
+
+    /// $2004 r/w
     oam_ram: [u8; 256],
 }
 
@@ -120,6 +158,28 @@ impl Ppu {
         nim
     }
 
+    /// $2002
+    ///
+    /// 7  bit  0
+    /// ---- ----
+    /// VSO. ....
+    /// |||| ||||
+    /// |||+-++++- Least significant bits previously written into a PPU register
+    /// |||        (due to register not being updated for this address)
+    /// ||+------- Sprite overflow. The intent was for this flag to be set
+    /// ||         whenever more than eight sprites appear on a scanline, but a
+    /// ||         hardware bug causes the actual behavior to be more complicated
+    /// ||         and generate false positives as well as false negatives; see
+    /// ||         PPU sprite evaluation. This flag is set during sprite
+    /// ||         evaluation and cleared at dot 1 (the second dot) of the
+    /// ||         pre-render line.
+    /// |+-------- Sprite 0 Hit.  Set when a nonzero pixel of sprite 0 overlaps
+    /// |          a nonzero background pixel; cleared at dot 1 of the pre-render
+    /// |          line.  Used for raster timing.
+    /// +--------- Vertical blank has started (0: not in vblank; 1: in vblank).
+    ///            Set at dot 1 of line 241 (the line *after* the post-render
+    ///            line); cleared after reading $2002 and at dot 1 of the
+    ///            pre-render line.
     pub fn status(&mut self) -> u8 {
         let mut status = self.control.as_u8() & 0x1F;
 
