c047a7f577736d9dea9953eda9098c68c52fce47
Federico Ravasio <ravasio.federico@gmail.com>
2019-05-18 12:03:08 +0000

    Added naive benchmark on step_frame.

---
+++ benches/frame.rs
@@ -0,0 +1,17 @@
+use criterion::*;
+use nes::Nes;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let rom = include_bytes!("../tests/roms/nestest.nes");
+
+    c.bench_function("ppu tile rendering", move |b| {
+        b.iter(|| {
+            let mut nes = Nes::from_buf(rom);
+
+            nes.step_frame();
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
